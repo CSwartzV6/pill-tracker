0279ef6f9f462ea8ac7c89ac6a8e543c4d501c54
Cody Swartz <cody.swartz@outlook.com>
2020-12-17 15:12:49 -0500

    Alpha Publish

---
+++ logic.js
@@ -0,0 +1,47 @@
+if (typeof(Storage) !== "undefined") {
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("pillsCount");
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+  
+  function pillsCounter() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.pillsCount) {
+        localStorage.pillsCount = Number(localStorage.pillsCount)+30;
+      } else {
+        localStorage.pillsCount = 1;
+      }
+      document.getElementById("result").innerHTML = localStorage.pillsCount;
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
+  function pillsCounterMinus() {
+   //show time last taken
+    if (typeof(Storage) !== "undefined") {
+        var timeNow = (new Date());
+        document.getElementById("demo").innerHTML = timeNow;
+       localStorage.timeTakenLast = timeNow;
+      if (localStorage.pillsCount) {
+        localStorage.pillsCount = Number(localStorage.pillsCount)-1;
+      } else {
+        localStorage.pillsCount = 1;
+      }
+      document.getElementById("result").innerHTML = localStorage.pillsCount;
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
+  function pillsCounterReset() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.pillsCount) {
+        localStorage.pillsCount = Number(localStorage.pillsCount)*0;
+      } else {
+        localStorage.pillsCount = 1;
+      }
+      document.getElementById("result").innerHTML = localStorage.pillsCount;
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
